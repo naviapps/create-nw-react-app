512efbb33ce4dff7df33d88955fd9d387bd2fc93
Haruki Fukui <haruki.fukui@naviapis.com>
2017-08-14 04:27:50 +0900

    update commit

---
+++ index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const chalk = require('chalk');
+
+const currentNodeVersion = process.versions.node;
+const semver = currentNodeVersion.split('.');
+const major = semver[0];
+
+if (major < 4) {
+  console.error(
+    chalk.red(
+      'You are running Node ' +
+      currentNodeVersion +
+      '.\n' +
+      'Create NW.js React App requires Node 4 or higher. \n' +
+      'Please update your version of Node.'
+    )
+  );
+  process.exit(1);
+}
+
+require('./createNwReactApp');
