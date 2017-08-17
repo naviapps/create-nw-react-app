e751324f7eb8342e345c97edc55ecfeae4c85ec2
Haruki Fukui <haruki.fukui@naviapis.com>
2017-08-17 11:05:49 +0900

    Publish
     - create-nw-react-app@0.0.4
     - nw-react-scripts@0.1.12

--- packages/create-nw-react-app/createNwReactApp.js
+++ packages/create-nw-react-app/createNwReactApp.js
@@ -1,24 +1,383 @@
-#!/usr/bin/env node
-
 'use strict';
 
+const validateProjectName = require('validate-npm-package-name');
 const chalk = require('chalk');
+const commander = require('commander');
+const fs = require('fs-extra');
+const path = require('path');
+const execSync = require('child_process').execSync;
+const spawn = require('cross-spawn');
+const semver = require('semver');
 
-const currentNodeVersion = process.versions.node;
-const semver = currentNodeVersion.split('.');
-const major = semver[0];
+const packageJson = require('./package.json');
 
-if (major < 4) {
-  console.error(
-    chalk.red(
-      'You are running Node ' +
-      currentNodeVersion +
-      '.\n' +
-      'Create NW.js React App requires Node 4 or higher. \n' +
-      'Please update your version of Node.'
-    )
+let projectName;
+
+const program = new commander.Command(packageJson.name)
+  .version(packageJson.version)
+  .arguments('<project-directory>')
+  .usage(`${chalk.green('<project-directory>')} [options]`)
+  .action(name => {
+    projectName = name;
+  })
+  .option('--verbose', 'print additional logs')
+  .allowUnknownOption()
+  .on('--help', () => {
+    console.log(`    Only ${chalk.green('<project-directory>')} is required.`);
+    console.log();
+    console.log(
+      `    If you have any problems, do not hesitate to file an issue:`
+    );
+    console.log(
+      `      ${chalk.cyan(
+        'https://github.com/naviapps/create-nw-react-app/issues/new'
+      )}`
+    );
+    console.log();
+  })
+  .parse(process.argv);
+
+if (typeof projectName === 'undefined') {
+  console.error('Please specify the project directory:');
+  console.log(
+    `  ${chalk.cyan(program.name())} ${chalk.green('<project-directory>')}`
+  );
+  console.log();
+  console.log('For example:');
+  console.log(`  ${chalk.cyan(program.name())} ${chalk.green('my-nw-react-app')}`);
+  console.log();
+  console.log(
+    `Run ${chalk.cyan(`${program.name()} --help`)} to see all options.`
   );
   process.exit(1);
 }
 
-require('./createNwReactApp');
+function printValidationResults(results) {
+  if (typeof results !== 'undefined') {
+    results.forEach(error => {
+      console.error(chalk.red(`  *  ${error}`));
+    });
+  }
+}
+
+const hiddenProgram = new commander.Command()
+  .option(
+    '--internal-testing-template <path-to-template>',
+    '(internal usage only, DO NOT RELY ON THIS) ' +
+    'use a non-standard application template'
+  )
+  .parse(process.argv);
+
+createApp(
+  projectName,
+  program.verbose,
+  hiddenProgram.internalTestingTemplate
+);
+
+function createApp(name, verbose, template) {
+  const root = path.resolve(name);
+  const appName = path.basename(root);
+
+  checkAppName(appName);
+  fs.ensureDirSync(name);
+  if (!isSafeToCreateProjectIn(root, name)) {
+    process.exit(1);
+  }
+
+  if (!semver.satisfies(process.version, '>=6.0.0')) {
+    console.log(
+      chalk.yellow(
+        `You are using Node ${process.version} so the project will be bootstrapped with an old unsupported version of tools.\n\n` +
+        `Please update to Node 6 or higher for a better, fully supported experience.\n`
+      )
+    );
+    process.exit(1);
+  }
+
+  const npmInfo = checkNpmVersion();
+  if (!npmInfo.hasMinNpm) {
+    if (npmInfo.npmVersion) {
+      console.log(
+        chalk.yellow(
+          `You are using npm ${npmInfo.npmVersion} so the project will be boostrapped with an old unsupported version of tools.\n\n` +
+          `Please update to npm 3 or higher for a better, fully supported experience.\n`
+        )
+      );
+    }
+    process.exit(1);
+  }
+
+  console.log(`Creating a new NW.js React app in ${chalk.green(root)}.`);
+  console.log();
+
+  const packageJson = {
+    name: appName,
+    version: '0.0.1',
+    private: true,
+  };
+  fs.writeFileSync(
+    path.join(root, 'package.json'),
+    JSON.stringify(packageJson, null, 2)
+  );
+  const originalDirectory = process.cwd();
+  process.chdir(root);
+
+  run(root, appName, verbose, originalDirectory, template);
+}
+
+function install(dependencies, verbose) {
+  return new Promise((resolve, reject) => {
+    const command = 'npm';
+    const args = [
+      'install',
+      '--save',
+      '--save-exact',
+      '--loglevel',
+      'error',
+    ].concat(dependencies);
+
+    if (verbose) {
+      args.push('--verbose');
+    }
+
+    const child = spawn(command, args, { stdio: 'inherit' });
+    child.on('close', code => {
+      if (code !== 0) {
+        reject({
+          command: `${command} ${args.join(' ')}`,
+        });
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+function run(
+  root,
+  appName,
+  verbose,
+  originalDirectory,
+  template
+) {
+  const packageName = 'nw-react-scripts';
+  const allDependencies = ['react', 'react-dom', packageName];
+
+  console.log('Installing packages. This might take a couple of minutes.');
+  console.log(
+    `Installing ${chalk.cyan('react')}, ${chalk.cyan(
+      'react-dom'
+    )}, and ${chalk.cyan(packageName)}...`
+  );
+  console.log();
+
+  install(allDependencies, verbose).then(
+    () => packageName
+  )
+    .then(packageName => {
+      checkNodeVersion(packageName);
+      setCaretRangeForRuntimeDeps(packageName);
+
+      const scriptsPath = path.resolve(
+        process.cwd(),
+        'node_modules',
+        packageName,
+        'scripts',
+        'init.js'
+      );
+      const init = require(scriptsPath);
+      init(root, appName, verbose, originalDirectory, template);
+    })
+    .catch(reason => {
+      console.log();
+      console.log('Aborting installation.');
+      if (reason.command) {
+        console.log(`  ${chalk.cyan(reason.command)} has failed.`);
+      } else {
+        console.log(chalk.red('Unexpected error. Please report it as a bug:'));
+        console.log(reason);
+      }
+      console.log();
+
+      const knownGeneratedFiles = [
+        'package.json',
+        'npm-debug.log',
+        'node_modules',
+      ];
+      const currentFiles = fs.readdirSync(path.join(root));
+      currentFiles.forEach(file => {
+        knownGeneratedFiles.forEach(fileToMatch => {
+          if (
+            (fileToMatch.match(/.log/g) && file.indexOf(fileToMatch) === 0) ||
+            file === fileToMatch
+          ) {
+            console.log(`Deleting generated file... ${chalk.cyan(file)}`);
+            fs.removeSync(path.join(root, file));
+          }
+        });
+      });
+      const remainingFiles = fs.readdirSync(path.join(root));
+      if (!remainingFiles.length) {
+        console.log(
+          `Deleting ${chalk.cyan(`${appName} /`)} from ${chalk.cyan(
+            path.resolve(root, '..')
+          )}`
+        );
+        process.chdir(path.resolve(root, '..'));
+        fs.removeSync(path.join(root));
+      }
+      console.log('Done.');
+      process.exit(1);
+    });
+}
+
+function checkNpmVersion() {
+  let hasMinNpm = false;
+  let npmVersion = null;
+  try {
+    npmVersion = execSync('npm --version').toString().trim();
+    hasMinNpm = semver.gte(npmVersion, '3.0.0');
+  } catch (err) {
+    // ignore
+  }
+  return {
+    hasMinNpm: hasMinNpm,
+    npmVersion: npmVersion,
+  };
+}
+
+function checkNodeVersion(packageName) {
+  const packageJsonPath = path.resolve(
+    process.cwd(),
+    'node_modules',
+    packageName,
+    'package.json'
+  );
+  const packageJson = require(packageJsonPath);
+  if (!packageJson.engines || !packageJson.engines.node) {
+    return;
+  }
+
+  if (!semver.satisfies(process.version, packageJson.engines.node)) {
+    console.error(
+      chalk.red(
+        'You are running Node %s.\n' +
+        'Create React App requires Node %s or higher. \n' +
+        'Please update your version of Node.'
+      ),
+      process.version,
+      packageJson.engines.node
+    );
+    process.exit(1);
+  }
+}
+
+function checkAppName(appName) {
+  const validationResult = validateProjectName(appName);
+  if (!validationResult.validForNewPackages) {
+    console.error(
+      `Could not create a project called ${chalk.red(
+        `"${appName}"`
+      )} because of npm naming restrictions:`
+    );
+    printValidationResults(validationResult.errors);
+    printValidationResults(validationResult.warnings);
+    process.exit(1);
+  }
+
+  const dependencies = ['react', 'react-dom', 'nw-react-scripts'].sort();
+  if (dependencies.indexOf(appName) >= 0) {
+    console.error(
+      chalk.red(
+        `We cannot create a project called ${chalk.green(
+          appName
+        )} because a dependency with the same name exists.\n` +
+        `Due to the way npm works, the following names are not allowed:\n\n`
+      ) +
+      chalk.cyan(dependencies.map(depName => `  ${depName}`).join('\n')) +
+      chalk.red('\n\nPlease choose a different project name.')
+    );
+    process.exit(1);
+  }
+}
+
+function makeCaretRange(dependencies, name) {
+  const version = dependencies[name];
+
+  if (typeof version === 'undefined') {
+    console.error(chalk.red(`Missing ${name} dependency in package.json`));
+    process.exit(1);
+  }
+
+  let patchedVersion = `^${version}`;
+
+  if (!semver.validRange(patchedVersion)) {
+    console.error(
+      `Unable to patch ${name} dependency version because version ${chalk.red(
+        version
+      )} will become invalid ${chalk.red(patchedVersion)}`
+    );
+    patchedVersion = version;
+  }
+
+  dependencies[name] = patchedVersion;
+}
+
+function setCaretRangeForRuntimeDeps(packageName) {
+  const packagePath = path.join(process.cwd(), 'package.json');
+  const packageJson = require(packagePath);
+
+  if (typeof packageJson.dependencies === 'undefined') {
+    console.error(chalk.red('Missing dependencies in package.json'));
+    process.exit(1);
+  }
+
+  const packageVersion = packageJson.dependencies[packageName];
+  if (typeof packageVersion === 'undefined') {
+    console.error(chalk.red(`Unable to find ${packageName} in package.json`));
+    process.exit(1);
+  }
+
+  makeCaretRange(packageJson.dependencies, 'react');
+  makeCaretRange(packageJson.dependencies, 'react-dom');
+
+  fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2));
+}
+
+function isSafeToCreateProjectIn(root, name) {
+  const validFiles = [
+    '.DS_Store',
+    'Thumbs.db',
+    '.git',
+    '.gitignore',
+    '.idea',
+    'README.md',
+    'LICENSE',
+    'web.iml',
+    '.hg',
+    '.hgignore',
+    '.hgcheck',
+  ];
+  console.log();
+
+  const conflicts = fs
+    .readdirSync(root)
+    .filter(file => !validFiles.includes(file));
+  if (conflicts.length < 1) {
+    return true;
+  }
+
+  console.log(
+    `The directory ${chalk.green(name)} contains files that could conflict:`
+  );
+  console.log();
+  for (const file of conflicts) {
+    console.log(`  ${file}`);
+  }
+  console.log();
+  console.log(
+    'Either try using a new directory name, or remove the files listed above.'
+  );
+
+  return false;
+}
