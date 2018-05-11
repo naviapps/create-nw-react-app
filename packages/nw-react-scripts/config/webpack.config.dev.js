7e47afc4414847b151ecb919dc8f3568a36ef95e
Haruki Fukui <haruki.fukui@naviapis.com>
2018-05-11 18:41:34 +0900

    fixed public_url

--- packages/nw-react-scripts/config/webpack.config.dev.js
+++ packages/nw-react-scripts/config/webpack.config.dev.js
@@ -29,7 +29,7 @@ const publicPath = `${protocol}://${host}:${port}/`;
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
-const publicUrl = '';
+const publicUrl = `${protocol}://${host}:${port}`;
 // Get environment variables to inject into our app.
 const env = getClientEnvironment(publicUrl);
 
